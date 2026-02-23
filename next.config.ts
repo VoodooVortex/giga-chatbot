ce082e77f77200c5b98c366361efbdc923ab9dcf
Pakkapon <Pakkapon2547@gmail.com>
2026-02-24 01:00:27 +0700

    feat: Update next.config.ts to set basePath for chat routing

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  basePath: "/chat",
+  // assetPrefix: "/chat", // เปิดถ้าเจอปัญหา asset path หลัง proxy
   /* config options here */
 };
 
